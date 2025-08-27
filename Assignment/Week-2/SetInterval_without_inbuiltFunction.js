b1f6bb1d8dc4af524ddb9857a684e9a3e9b3583f
ashish8112 <ashish233227shukla@gmail.com>
2025-08-27 12:25:13 +0530

    assignment

---
+++ Assignment/Week-2/SetInterval_without_inbuiltFunction.js
@@ -0,0 +1,38 @@
+let count =1;
+function callback()
+{
+    console.clear();
+    console.log(count+" sec passed");
+    count++;
+    setTimeout(callback,1000);
+}
+setTimeout(callback,1000);
+
+
+
+//  let count=0; 
+//  let i=1; 
+//  while(i<=10)    // This will not work becuase unitl call stack is empty async function can't excute in call stack it will stay in callback Queue until call stack is empty
+//  {
+//      count++  
+//    setTimeout(callback,1000);
+//    i++
+//  }
+//  function callback(count)
+//  {
+//     console.log(count);
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment/Week-2/setInterval.js
@@ -0,0 +1,8 @@
+let count=1;
+function counter(){
+    console.clear(); // for clearing the console else all output will be shown like 1 2 3 4 ....
+    console.log(count);
+    count++;
+    
+}
+setInterval(counter,1000);
