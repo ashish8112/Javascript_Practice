ab8b114e36c79be6a3541dd615a440de3d8f9695
ashish8112 <ashish233227shukla@gmail.com>
2025-09-03 16:12:12 +0530

    JS_Upgradation

--- Async/setInterval.js
+++ Async/setInterval.js
@@ -1,8 +1,11 @@
 let count=1;
+let stop;
 function counter(){
     console.clear(); // for clearing the console else all output will be shown like 1 2 3 4 ....
     console.log(count);
     count++;
+    if(i==60)
+        clearInterval(stop);// This will stop the counter when reaches to 60 second.
     
 }
-setInterval(counter,1000); // will call continously to function counter after each 1sec interval.
+stop=setInterval(counter,1000); // will call continously to function counter after each 1sec interval.
