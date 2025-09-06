cfa7b86de2ffc6fcae3cc97c5fc7d3cbf7112e25
ashish8112 <ashish233227shukla@gmail.com>
2025-09-06 11:17:48 +0530

    Arg_function

--- Function/02_function_Arg.js
+++ Function/02_function_Arg.js
@@ -5,25 +5,26 @@ let ashish= function() //Assignment Function
 
 ashish();
 
-    //Sending function as argument
+
 
     function sum(a,b)
     {
         return a+b;
     }
 
-    function caller(a,b,parameter) // Here i am receiving parameter = sum or we can call taking function as argument without that parameter,means sending function as argument without giving argument
+    function caller(a,b,parameter) // Here i am receiving, parameter = sum or we can call taking function as argument without parameter of function,means sending function as argument without giving argument
     {
        return parameter(a,b);
     }
-    let Sum = caller(5,10,sum);
+    let Sum = caller(5,10,sum);     //Sending function as argument
+
     console.log(Sum);
 
-    // returning function from and storing in assignment function
+    
 
-    function greet (firstName)
+    function greet (firstName) // This function will return function .
     {
-        return function() // just like we return a; so we can return function
+        return function() // just like we return ;variables or numbers,  so we can return function as well.
         {
             console.log("hello "+firstName); // this will only execute when this function is stored in another function X and when we call function x() that will execute
         }
