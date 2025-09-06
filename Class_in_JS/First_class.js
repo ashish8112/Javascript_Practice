6b0d0004ac81003168b074f0341ef56eabf63f1f
ashish8112 <ashish233227shukla@gmail.com>
2025-09-06 11:22:19 +0530

    Asss

--- Class_in_JS/First_class.js
+++ Class_in_JS/First_class.js
@@ -5,7 +5,7 @@ class ashish{
         this.Name=name;  // Here I created new variable as well Name and this for variable of this instace of class (object)
         this.Age=age;
     }
-    Umar()
+    Umar() // In class we don't use function keyword to create function inside the class.
     {
 
         console.log(this.Name+" is  "+this.Age+" year old"); //Note only we can this.variable name in entire class not only with variable name;
@@ -19,3 +19,4 @@ let obj=new ashish("Ashish",23,"White");
 obj.Umar();
 let color=obj.paint();
 console.log(color);
+console.log(obj.color); // we can access variable of class using object . variable name ;
