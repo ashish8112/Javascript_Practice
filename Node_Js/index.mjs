a63e35fc6813937e6772db351d1d674ecd24fae8
ashish8112 <ashish233227shukla@gmail.com>
2025-09-20 11:17:29 +0530

    Node Js

---
+++ Node_Js/index.mjs
@@ -0,0 +1,7 @@
+//Importing External libraries in the program for that we need to download or install it.
+import chalk from 'chalk'; // In js we were doing || let fs=require("fs"); for importing libraries.
+console.log(chalk.blue("Hello Vikas R"));
+console.log(chalk.green("Hello Daksh"));
+console.log(chalk.yellow("Hello")+"world"+chalk.red("!"));
+console.log(chalk.blue.bgRed.bold("Hello world!"));
+console.log(chalk.red.underline("Ashish"));
