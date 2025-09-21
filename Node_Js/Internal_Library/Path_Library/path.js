3367788927022f381854954cc204c41f93ad646a
ashish8112 <ashish233227shukla@gmail.com>
2025-09-21 12:59:20 +0530

    npm install

--- Node_Js/Internal_Library/Path_Library/path.js
+++ Node_Js/Internal_Library/Path_Library/path.js
@@ -1,3 +1,4 @@
+// For Internal Library we use require and for external library we use import keyword.
 const path=require("path");
 console.log(__dirname);
 console.log(path.join("C:/")+__dirname); // It is used to join path 
