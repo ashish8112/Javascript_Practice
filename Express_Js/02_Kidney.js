0b879b14ac2c0b1fcd0168a3803604b67b7ca2f6
ashish8112 <ashish233227shukla@gmail.com>
2025-09-28 19:24:34 +0530

    Backend

--- Express_Js/02_Kidney.js
+++ Express_Js/02_Kidney.js
@@ -16,8 +16,8 @@ const user = [{
 
 // app.listen(3000);
 
+app.use(express.json()); 
 app.get("/",function(req,res){
-    let johnKidneys=user[0].kidneys;
     const numberOfKidneys = user[0].kidneys.length;
     let numberOfHealthyKidneys=0;
     for(let i=0;i<numberOfKidneys;i++){
@@ -25,8 +25,64 @@ app.get("/",function(req,res){
                 numberOfHealthyKidneys++;
             }
     const numberOfUnhealthyKidneys=numberOfKidneys-numberOfHealthyKidneys;
-    res.json({johnKidneys,numberOfHealthyKidneys,numberOfUnhealthyKidneys});
+    res.json({numberOfKidneys,numberOfHealthyKidneys,numberOfUnhealthyKidneys});
 
 });
 
+app.post("/",function(req,res){
+    const isHealthy=req.body.isHealthy; // This is body query so in postman i need to write body is { isHealthy= true } for healthy and false for unhealthy.
+    user[0].kidneys.push({
+        healthy:isHealthy // we can give only boolean input because healthy is global variable in user array.
+    })
+    res.json({      // JSON -> JavaScript Object Notation.
+        msg:isHealthy+" Kidney pushed!"
+    });
+});
+
+//
+app.put("/",function(req,res){ // There is no body query so although anything written in body will it will affect this .
+    for(let i=0;i<user[0].kidneys.length;i++)// This function will convert all unhealthy to healthy kidney.
+    {
+        if(user[0].kidneys[i].healthy!==true)
+            user[0].kidneys[i].healthy=true;
+    }
+    res.json({});
+})
+
+//Only if atleast one unhealthy kidney is there do this, else return 411.
+app.delete("/",function(req,res){
+    let checkUnhealthy=atleastOneunhealthy();
+    if(checkUnhealthy>0){
+    const newKidney =[];
+    for(let i=0;i<user[0].kidneys.length;i++)
+    {
+        if(user[0].kidneys[i].healthy===true)
+            newKidney.push({
+                    healthy:true
+        })
+    }
+    user[0].kidneys=newKidney;
+    res.json({
+        msg:"Unhealthy Kidney Removed!"
+    });
+                            }
+    else{
+        res.status(411).json({
+            msg:"You have no unhealthy Kidney to remove"
+        })
+    }
+})
+function atleastOneunhealthy()
+{
+    let unhealthy=0;
+    for(let i=0;i<user[0].kidneys.length;i++)
+    {
+        if(user[0].kidneys[i].healthy===false)
+        {
+            unhealthy=1;
+            return unhealthy;
+        }
+        
+    }
+}
 app.listen(3000);
