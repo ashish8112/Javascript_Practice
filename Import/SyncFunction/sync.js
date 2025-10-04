1df3caac7476461e21b3891e17ba32f20384100f
ashish8112 <ashish233227shukla@gmail.com>
2025-10-04 18:31:24 +0530

    Removed

--- Import/SyncFunction/sync.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-const contents = fs.readFileSync("a.txt", "utf-8");
-console.log(contents);
-
-const contents2 = fs.readFileSync("demo.txt", "utf-8");
-console.log(contents2);
