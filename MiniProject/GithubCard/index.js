c038d3207cbefebaf4dcdb08f25be4cc2784258b
ashish8112 <ashish233227shukla@gmail.com>
2025-10-21 17:43:27 +0530

    Github Profile Generator

--- MiniProject/GithubCard/index.js
+++ MiniProject/GithubCard/index.js
@@ -1,14 +1,50 @@
-const url="https://api.github.com/users/ashish8112";
+document.querySelector("button").addEventListener("click",takeInput);
+const baseUrl="https://api.github.com/users/";
+function takeInput()
+{
+    const input=document.querySelector("input").value.trim();//trim removes space from start and end.
+    if(input!="")
+    {
+      const url=baseUrl+input;
+      apiSearch(url);  
+    }
+    else{
+        alert("Please enter your username!");
+        return;
+    }
+}
+
+
+// const url="https://api.github.com/users/ashish8112";
+
+function apiSearch(url){
 const xhr= new XMLHttpRequest();
 xhr.open("GET",url);
-xhr.onreadystatechange = function(){
+
+//xhr.onreadystatechange → XHR object khud call karta hai, jab bhi readyState change hota hai.
+//Matlab tu bas function assign karta hai, aur XHR engine internally monitor karta hai ki request ka state change hua ya nahi.
+//Browser ke JS engine me XHR object ka internal event system hai jo readyState change hone par check karta hai:
+//Agar function assigned hai → call ho jaata hai automatically
+//Tu explicitly call nahi kar raha
+//Isi liye hum bolte hain callback function — kyunki function kab execute hoga, wo decide XHR engine karega, na ki hum.
+xhr.onreadystatechange = function(){    // event handler when the state of xml changes it runs this function.
     if(xhr.readyState===4)
     {
         const data=JSON.parse(this.responseText);
+        if(data.message==="Not Found")
+        {
+            alert("⚠️ User not found!");
+            return;
+        }
+       else if (data.message === "API rate limit exceeded for 103.48.144.5. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)") {
+                alert("⚠️ You reached GitHub API limit! Wait for 1 hour or use a token.");
+                return;
+        }
         render(data);
     }
 }
 xhr.send();
+}
 function render(data)
 {
     const avatar=data.avatar_url;
@@ -35,6 +71,7 @@ function render(data)
     div.style.alignItems="center";
     div.style.justifyContent="space-between";
     div.style.overflow="auto";
+    div.style.margin="20px 20px";
 
     //styling div of img 
     div01.style.height="80%";
