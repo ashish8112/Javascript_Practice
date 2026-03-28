b3551ac2e27bea79274fc2dab863a71607080697
ashish8112 <ashish233227shukla@gmail.com>
2026-03-29 01:39:17 +0530

    Updated

--- Claude_Learning_MERN/Project/adusa-frontend/src/App.jsx
+++ Claude_Learning_MERN/Project/adusa-frontend/src/App.jsx
@@ -102,34 +102,55 @@
   // }
 
 
-  import {useState} from 'react';
-  function App()
-  {
-    const[count,Counter]=useState(0);
-    function increaseCounter()
-    {
-      Counter(count+1);
-    }
-    function decreaseCounter()
-    {
-      Counter(count-1);
-    }
+  // import {useState} from 'react';
+  // function App()
+  // {
+  //   const[count,Counter]=useState(0);
+  //   function increaseCounter()
+  //   {
+  //     Counter(count+1);
+  //   }
+  //   function decreaseCounter()
+  //   {
+  //     Counter(count-1);
+  //   }
 
-    return (
-      <div>
-        <h1>{count}</h1>
-        <button onClick={increaseCounter}>Increase Count</button> 
-        <br></br>
-        <button onClick={decreaseCounter}>Decrease Count</button>
-        <br></br>
-        <button onClick={()=>Counter(0)}>Reset Count</button>
-        {/* Note we cannot do onClick={Counter(count+1)} it is not HTML , it is jsx it will excute instantly and will cause infinite loop */}
-        {/* Instead create arrow function  */}
-        <br></br>
-        <button onClick={function(){Counter(0)}}>Another reset Count with Traditional function</button>
-      </div>
-    )
-  }
+  //   return (
+  //     <div>
+  //       <h1>{count}</h1>
+  //       <button onClick={increaseCounter}>Increase Count</button> 
+  //       <br></br>
+  //       <button onClick={decreaseCounter}>Decrease Count</button>
+  //       <br></br>
+  //       <button onClick={()=>Counter(0)}>Reset Count</button>
+  //       {/* Note we cannot do onClick={Counter(count+1)} it is not HTML , it is jsx it will excute instantly and will cause infinite loop */}
+  //       {/* Instead create arrow function  */}
+  //       <br></br>
+  //       <button onClick={function(){Counter(0)}}>Another reset Count with Traditional function</button>
+  //     </div>
+  //   )
+  // }
 
+function App()
+{
+  const students=[
+    {_id:"1",name:"Ashish",branch:"MCA"},
+    {_id:"2",name:"Riya",branch:"CSE"},
+    {_id:"3",name:"Rahul",branch:"ECE"}
+  ];
+
+  return(
+    <div>
+      {
+        students.map((student)=>(
+          <div key={student._id}>
+          <h2>{student.name}</h2>
+          <p>{student.branch}</p>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
 
 export default App;
