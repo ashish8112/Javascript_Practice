9e601534f7ce3463a38e925837cbc386fec88bf7
ashish8112 <ashish233227shukla@gmail.com>
2026-03-28 23:51:34 +0530

    Added App functionaliry

--- Claude_Learning_MERN/Project/adusa-frontend/src/App.jsx
+++ Claude_Learning_MERN/Project/adusa-frontend/src/App.jsx
@@ -88,19 +88,48 @@
 // }
 
 
-  import { useState } from "react";
+  // import { useState } from "react";
+  // function App()
+  // {
+  //   const students= ["Ashish","Rahul","Manish","Deep"];
+  //   return(
+  //     <div>
+  //      {students.map((student,index)=> { return <div key ={index}>{student}</div>  }
+  //          //automatic return becuase of arrow function ()=>"ashish" but when you use ()=>{return ashish} need return for curly
+  //      )}
+  //     </div>
+  //   )
+  // }
+
+
+  import {useState} from 'react';
   function App()
   {
-    const students= ["Ashish","Rahul","Manish","Deep"];
-    return(
+    const[count,Counter]=useState(0);
+    function increaseCounter()
+    {
+      Counter(count+1);
+    }
+    function decreaseCounter()
+    {
+      Counter(count-1);
+    }
+
+    return (
       <div>
-       {students.map((student,index)=> { return <div key ={index}>{student}</div>  }
-           //automatic return becuase of arrow function ()=>"ashish" but when you use ()=>{return ashish} need return for curly
-       )}
+        <h1>{count}</h1>
+        <button onClick={increaseCounter}>Increase Count</button> 
+        <br></br>
+        <button onClick={decreaseCounter}>Decrease Count</button>
+        <br></br>
+        <button onClick={()=>Counter(0)}>Reset Count</button>
+        {/* Note we cannot do onClick={Counter(count+1)} it is not HTML , it is jsx it will excute instantly and will cause infinite loop */}
+        {/* Instead create arrow function  */}
+        <br></br>
+        <button onClick={function(){Counter(0)}}>Another reset Count with Traditional function</button>
       </div>
     )
   }
-
 
 
 export default App;
