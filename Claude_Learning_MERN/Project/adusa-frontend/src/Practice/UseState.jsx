8be4bbdb9aec91c613cb323348893d9800f5f833
ashish8112 <ashish233227shukla@gmail.com>
2026-04-02 20:32:00 +0530

    UseState Practice

--- Claude_Learning_MERN/Project/adusa-frontend/src/Practice/UseState.jsx
+++ Claude_Learning_MERN/Project/adusa-frontend/src/Practice/UseState.jsx
@@ -4,7 +4,7 @@ function UseStatePractice()
     const[count,setCount]=useState(0);//useState initialise count variable to 0 and setCount is setterFunction created by useState
    function handleClick(){
    // count = count+1;// unnecessary use setCount(count+1); and as well as if it is directly in component it will cause infinite loop
-    setCount(count+1);//when we increase count value react use to render new value || It is used to re render 
+    setCount(count+1); //when we increase count value react use to render new value || It is used to re render 
    }
     return(
         <div>
