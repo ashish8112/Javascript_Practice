90096806126ffa327bb322a93a515145c91826b9
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-04-10 19:44:48 +0530

    Update First_class.js

--- Class_in_JS/First_class.js
+++ Class_in_JS/First_class.js
@@ -2,7 +2,7 @@ class ashish{
     constructor(name,age,color)//Although if i don't create constructor it will run because of creation of default constructor by JS Engine.
     {
         this.color=color;
-        this.Name=name;  // Here I created new variable as well Name and this for variable of this instace of class (object)
+        this.Name=name;  // Here I created new variable as well Name and this for variable of this instace of class (object). 
         this.Age=age;
     }
     Umar() // In class we don't use function keyword to create function inside the class.
