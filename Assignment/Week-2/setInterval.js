cb44a3089a50535efde287d0220ab0fada91d118
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-05-08 20:02:53 +0530

    Update setInterval.js

--- Assignment/Week-2/setInterval.js
+++ Assignment/Week-2/setInterval.js
@@ -1,6 +1,6 @@
 let count=1;
 function counter(){
-    console.clear(); // for clearing the console else all output will be shown like 1 2 3 4 ....
+    console.clear(); // for clearing the console else all output will be shown like 1 2 3 4 ......
     console.log(count);
     count++;
     
