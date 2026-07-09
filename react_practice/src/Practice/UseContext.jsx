d0bae34a9a131d00f0270496c15c68f523ba8e0d
Ashish <ashish233227shukla@gmail.com>
2026-07-09 18:59:45 +0530

    Added adusa frontend practice files

---
+++ react_practice/src/Practice/UseContext.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+export const UserContext = createContext();//Created Context (Global Variable),createContext return object 
+//UserContext = Context Object (Provider + Consumer) 
+//PascalCase naming Convention, not a component.
